bc856bc56cb08fd073540e0538c6ffe2d4326e8f
Vic Yang <vicyangworld@gmail.com>
2018-01-10 15:42:47 +0800

    Create myString.h

---
+++ DataStructure/String/myString.h
@@ -0,0 +1,41 @@
+#ifndef _H_MYSTRING_
+#define _H_MYSTRING_ value
+
+const int MaxSize = 100;
+class myString
+{
+private:
+	int m_ncurlen;
+	char *m_pstr;
+	int *m_pnext;
+public:
+	myString(const myString & copy);
+	myString(const char *init);
+	myString();
+	~myString(){
+		delete [] m_pstr;
+	}
+
+	int Length() const{
+		return m_ncurlen；
+	}
+	bool Find(myString part) const;
+	char* GetBuffer() const;
+	myString& operator()(int pos, int len);
+	bool operator==(const myString cmp_str) const;
+	bool operator!=(const myString cmp_str) const;
+	bool operator<(const myString cmp_str) const;
+	bool operator>(const myString cmp_str) const;
+	bool operator!() const{
+		return m_ncurlen==0;
+	}
+
+	myString& operator=(const myString& copy);
+	myString operator+=(const myString& add);
+	char& operator[](int i);
+	friend ostream& operator<<(ostream&, myString&);
+	friend istream& operator>>(istream&, myString&);
+private:
+	void next();
+};
+#endif
