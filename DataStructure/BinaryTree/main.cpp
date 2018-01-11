483e90c17a4a9ca2961692dae03ad393d4a019d1
Vic Yang <vicyangworld@gmail.com>
2018-01-11 17:06:15 +0800

    Create main.cpp

---
+++ DataStructure/BinaryTree/main.cpp
@@ -0,0 +1,47 @@
+#include <iosteram>
+#include "BinaryTree.h"
+
+using std::cout;
+using std::endl;
+using std::ostream;
+using std::isteram;
+
+itn main(int argc, char const *argv[])
+{
+	BinaryTree<int> tree(-1);
+	int init[30] = {
+		17,6,22,29,14,0,21,13,27,18,
+		2,28,8,26,3,12,20,4,9,
+		23,15,1,11,5,19,24,16,7,10,25
+	};
+	for (int i = 0; i < 30; ++i)
+	{
+		tree.Insert(init[i]);
+	}
+	cout<<tree<<endl;
+	cout<<"-----------------------------------------"<<endl;
+
+	cout<<"The parent of 20 :";
+	cout<<tree.GetParent(tree.Find(20))->GetData()<<endl;
+	cout<<"the right side of 15: ";
+	cout<<tree.Find(15)->GetRight()->GetData()<<endl;
+
+	cout<<"size = "<<tree.GetSize()<<endl;
+	cout<<"height="<<tree.GetHeight()<<endl;
+
+	tree.InOrder();
+	cout<<endl<<endl;
+	tree.PreOrder();
+	cout<<endl<<endl;
+	tree.PostOrder();
+	cout<<endl<<endl;
+
+	BinaryTree<int> tree2 = tree;
+	cout<<tree2<<endl;
+
+	cout<<"-------------------------------------------"<<endl;
+	cout<<tree2.GetParent(tree2.Find(20))->GetData()<<endl;
+	cout<<(tree==tree2)<<endl;
+
+	return 0;
+}
